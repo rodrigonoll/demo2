8d0498a35d8e5342cb666c508e774c1d99c702fa
Rodrigo Noll <rodrigo.noll@canoas.ifrs.edu.br>
2018-10-10 00:35:02 -0300

    aa

---
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -0,0 +1,53 @@
+package com.example.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
+
+@SpringBootApplication
+public class Demo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args);
+    }
+}
+
+
+@RestController
+class HomeController{
+
+    private static final String MESSAGE ="<getCurriculoCompactado>\n" +
+            "         <id>0917360107022796</id>\n" +
+            "      </   getCurriculoCompactado>";
+
+    private final WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
+
+    public void setDefaultUri(String defaultUri) {
+        webServiceTemplate.setDefaultUri(defaultUri);
+    }
+
+    // send to an explicit URI
+    public void customSendAndReceive() {
+        StreamSource source = new StreamSource(new StringReader(MESSAGE));
+        StreamResult result = new StreamResult(System.out);
+        webServiceTemplate.sendSourceAndReceiveToResult("http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo",
+                source, result);
+    }
+
+
+    @GetMapping("")
+    public void home(){
+        customSendAndReceive();
+    }
+}

373f0dca43008223964851c8a92de4c9cfcef52f
Rodrigo Noll <rodrigo.noll@canoas.ifrs.edu.br>
2018-10-11 09:33:07 -0300

    aa

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -9,6 +9,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +19,16 @@ import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.context.DefaultMessageContext;
 import org.springframework.ws.context.MessageContext;
 import org.springframework.ws.soap.saaj.SaajSoapMessage;
+import org.w3c.dom.Document;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.soap.*;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
@@ -36,6 +48,8 @@ public class Demo2Application {
 @RestController
 class HomeController{
 
+    private final String url = "http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo";
+
     private static final String MESSAGE ="<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ws=\"http://ws.servico.repositorio.cnpq.br/\">\n" +
             "   <soapenv:Header/>\n" +
             "   <soapenv:Body>\n" +
@@ -51,26 +65,94 @@ class HomeController{
         webServiceTemplate.setDefaultUri(defaultUri);
     }
 
-    // send to an explicit URI
-    public void customSendAndReceive() {
+    public SOAPEnvelope createSoapEnvelope() throws SOAPException {
+        MessageFactory factory = MessageFactory.newInstance();
+        SOAPMessage message = factory.createMessage();
+        SOAPPart soapPart = message.getSOAPPart();
+        SOAPEnvelope envelope = null;
+        try {
+            envelope = soapPart.getEnvelope();
+            SOAPBody body = envelope.getBody();
 
+            QName bodyName = new QName("http://ws.servico.repositorio.cnpq.br/","xmlns","ws");
+            SOAPBodyElement bodyElement = body.addBodyElement(bodyName);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document = db.newDocument();
+
+            // Marshal the Object to a Document
+            JAXBContext jaxbContext = JAXBContext.newInstance(String.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            JAXBElement elem =new JAXBElement(
+                    new QName("getCurriculoCompactado", "ws"), String.class, "<id>0917360107022796</id>");
+            marshaller.marshal(elem, document);
+
+
+            body.addDocument(document);
+            //body.setValue(MESSAGE);
+            return envelope;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return envelope;
+    }
+
+
+    public void customSendAndReceive() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+//        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+//        map.add("user", authentication.getName());
+//        map.add("password", authentication.getCredentials().toString());
+
+        HttpEntity<String> request = new HttpEntity(MESSAGE);
+
+        String remoteAuth = restTemplate.postForObject(url, request ,String.class);
+
+//        JsonElement jelement = new JsonParser().parse(remoteAuth);
+//        return jelement.getAsJsonObject().get("result").getAsString();
+    }
+
+    // send to an explicit URI
+    public void customSendAndReceive2() {
+
+
+        MultiValueMap headers = new HttpHeaders();
+        headers.set(HttpHeaders.ACCEPT,"text/xml;charset=UTF-8");
+        headers.set(HttpHeaders.CONTENT_TYPE,"text/xml;charset=UTF-8");
+        headers.set(HttpHeaders.ACCEPT_ENCODING,"gzip,deflate");
+        headers.set("SOAPAction", "SOAPAction:");
+        HttpEntity<SOAPEnvelope> soapEntity = null;
+        try {
+            soapEntity = new HttpEntity<>(createSoapEnvelope(),headers);
+        } catch (SOAPException e) {
+            e.printStackTrace();
+        }
 
         RestTemplate restTemplate =  new RestTemplate();
         //Create a list for the message converters
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
-        //Add the String Message converter
-        messageConverters.add(new StringHttpMessageConverter());
-        //Add the message converters to the restTemplate
-        restTemplate.setMessageConverters(messageConverters);
+//        List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+//        //Add the String Message converter
+//        messageConverters.add(new StringHttpMessageConverter());
+//        //Add the message converters to the restTemplate
+//        restTemplate.setMessageConverters(messageConverters);
 
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_XML);
-        HttpEntity<String> request = new HttpEntity<String>(MESSAGE, headers);
+        //HttpHeaders headers = new HttpHeaders();
+        //headers.setContentType(MediaType.APPLICATION_XML);
+        //HttpEntity<String> request = new HttpEntity<String>(MESSAGE, headers);
 
-        final ResponseEntity<String> response = restTemplate.postForEntity("http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo", request, String.class);
+        ResponseEntity<SOAPEnvelope> future=
+                restTemplate.postForEntity("http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo"
+                        ,soapEntity, SOAPEnvelope.class);
 
-        System.out.println(response);
+
+        //final ResponseEntity<String> response = restTemplate.postForEntity("http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo", request, String.class);
+
+//        System.out.println(future);
 //
 //
 //        StreamSource source = new StreamSource(new StringReader(MESSAGE));
